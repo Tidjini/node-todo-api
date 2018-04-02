595fb0b58618c333c73a0e13d4d5293a0bd6e9a4
Tidjini_Messaoudi <dy.31@hotmail.fr>
2018-04-02 12:08:23 +0100

    Initial Commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,21 @@
+const { MongoClient } = require("mongodb");
+const mongoURL = "mongodb://tidjini:pass@ds213759.mlab.com:13759/node-todo-api";
+
+MongoClient.connect(mongoURL, (err, db) => {
+  if (err) return console.log("Unable to connect to the database server api");
+
+  console.log("Connect with success :-)");
+
+  db.collection("Todos").insertOne(
+    {
+      text: "Something todo",
+      complited: false
+    },
+    (err, result) => {
+      if (err) return console.log("Unable to store to database");
+      console.log(result);
+    }
+  );
+
+  db.close();
+});
