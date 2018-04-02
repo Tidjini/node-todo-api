8cb55b509e10c8a30231eb6627830f6318da288d
Tidjini_Messaoudi <dy.31@hotmail.fr>
2018-04-02 16:23:27 +0100

    Test GET /todos route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,23 +4,36 @@ const request = require("supertest");
 const { app } = require("../server");
 const { Todo } = require("../models/Todo");
 
+const todos = [
+  {
+    text: "First Test Todo"
+  },
+  {
+    text: "Second Test Todo"
+  }
+];
+
+const TIME_OUT = 0;
+//remove before each done method
+beforeEach(done => {
+  //init the time out
+  //this.timeout(10000);
+  setTimeout(done, TIME_OUT);
+  //
+  Todo.remove({})
+    .then(() => {
+      //for getting some data
+      return Todo.insertMany(todos);
+    })
+    .then(() => done());
+});
+
 //TO regroup tests (same categorie)
 describe("POST /todos", () => {
   //this.timeout(10000);
-
-  //remove before each done method
-  beforeEach(done => {
-    //init the time out
-    //this.timeout(10000);
-    setTimeout(done, 0);
-
-    Todo.remove({}).then(() => {
-      done();
-    });
-  });
   it("should create a new todo", done => {
     //init the time out
-    setTimeout(done, 0);
+    setTimeout(done, TIME_OUT);
 
     const text = "Test";
 
@@ -36,7 +49,7 @@ describe("POST /todos", () => {
       .end((err, res) => {
         if (err) return done(err);
         //get all todos to check if the todo is realy stored
-        Todo.find()
+        Todo.find({ text })
           .then(todos => {
             //expecting that we added just one // NOTE: GOTO beforeEach if your already add data (init db to Collection)
             expect(todos.length).toBe(1);
@@ -45,12 +58,12 @@ describe("POST /todos", () => {
             //call done method to out the result
             done();
           })
-          .catch(err => done(e));
+          .catch(err => done(err));
       });
   });
 
   it("should not create a todo with invalid body request", done => {
-    setTimeout(done, 0);
+    setTimeout(done, TIME_OUT);
     request(app)
       .post("/todos")
       .send({})
@@ -61,12 +74,23 @@ describe("POST /todos", () => {
         Todo.find()
           .then(todos => {
             //expecting that we added just one // NOTE: GOTO beforeEach if your already add data (init db to Collection)
-            expect(todos.length).toBe(0);
-
+            expect(todos.length).toBe(2);
             //call done method to out the result
             done();
           })
-          .catch(err => done(e));
+          .catch(err => done(err));
       });
+  });
+});
+describe("GET /todos", () => {
+  it("should get all todos list", done => {
+    setTimeout(done, TIME_OUT);
+    request(app)
+      .get("/todos")
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
   });
 });
